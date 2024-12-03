130cc5d0109673da7fc3a45ca0b2fbad783b547d
sem1on <sgorlov21@gmail.com>
2024-12-03 22:51:36 +0300

    add OpenPageStep

--- src/test/java/steps/OpenPageStep.java
+++ src/test/java/steps/OpenPageStep.java
@@ -1,4 +1,13 @@
 package steps;
 
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class OpenPageStep {
+
+    @Step("Открытие стартовой страницы")
+    public void openMainPage(String url) {
+        open(url);
+    }
 }
