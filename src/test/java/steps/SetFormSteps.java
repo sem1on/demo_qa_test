1d9bfac269c899a35f2a74805f0de83c2a8ca201
sem1on <sgorlov21@gmail.com>
2024-12-03 23:05:02 +0300

    add SetFormSteps

--- src/test/java/steps/SetFormSteps.java
+++ src/test/java/steps/SetFormSteps.java
@@ -1,4 +1,34 @@
 package steps;
 
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class SetFormSteps {
+
+    @Step("Ввести имя")
+    public void setFirstName (String firstName) {
+        $("#firstName").setValue(firstName);
+    }
+
+    @Step("Ввести фамилию")
+    public void setLastName (String lastName) {
+        $("#lastName").setValue(lastName);
+    }
+
+    @Step("Ввести електронную почту")
+    public void setEmail (String email) {
+        $("#userEmail").setValue(email);
+    }
+
+    @Step("Ввести номер телефона")
+    public void setPhone (String phoneNumber) {
+        $("#userNumber").setValue(phoneNumber);
+    }
+
+    @Step("Ввести адрес")
+    public void setAddress(String address) {
+        $("#currentAddress").setValue(address);
+    }
+
 }
