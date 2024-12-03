d6638f00e51bb184ec0b8b07de9ac58c0c8777b7
sem1on <sgorlov21@gmail.com>
2024-12-03 23:57:13 +0300

    add VerificationFormSteps

--- src/test/java/steps/VerificationFormSteps.java
+++ src/test/java/steps/VerificationFormSteps.java
@@ -1,4 +1,22 @@
 package steps;
 
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
 public class VerificationFormSteps {
+
+    @Step("Проверка открытия модального окна")
+    public void verifyModalAppears() {
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    }
+
+    @Step("Проверка введенных значений")
+    public void verifyResult(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent().shouldHave(text(value));
+    }
 }
