34415bd6b275d8d7aed28229fd969563b4917be6
sem1on <sgorlov21@gmail.com>
2024-12-03 22:40:22 +0300

    add tests

---
+++ src/test/java/tests/RegistrationFormTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class RegistrationFormTest extends TestBase {
+
+    @Test
+    void formTest() {
+
+    }
+ }
---
+++ src/test/java/tests/RegistrationFormTestWithFaker.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class RegistrationFormTestWithFaker  extends TestBase {
+
+    @Test
+    void formTestWithFaker() {
+
+    }
+}
